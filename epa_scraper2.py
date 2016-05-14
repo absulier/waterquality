bf727b48506b077e2ab830ad2409cf0110e79fb7
Adam Sulier <absulier@gmail.com>
2016-05-14 11:00:18 -0400

    finished scraper, tested on DC data

--- epa_scraper2.py
+++ epa_scraper2.py
@@ -3,28 +3,48 @@ import urllib
 import pandas as pd
 from bs4 import BeautifulSoup
 
-#creating the soup
-url = 'https://oaspub.epa.gov/enviro/sdw_report_v3.first_table?pws_id=DC0000007&state=DC&source=Surface%20water%20purchased&population=12499&sys_num=0'
-page=urllib.urlopen(url).read()
-soup = BeautifulSoup(page, "html.parser")
+sdwis=pd.read_csv('dctest.csv')
+#function takes an index from the dataframe of water treatment plants
+#uses that index to find the wid and link to page on violations
+#creats two dataframes of health violations and reporting violations
+def viodata(index):
+    #creating the soup and gathering wid
+    wid=sdwis.wid[index]
+    url ='https:' + sdwis.links[index]
+    page=urllib.urlopen(url).read()
+    soup = BeautifulSoup(page, "html.parser")
 
-#goes into the page and fund the tables of health related violations
-allhvio=[]
-allmrvio=[]
-for table in soup.findAll('table', {'class':'result2'}):
-    count=0
-    for item in table.findAll('th'):
-        count+=1
-    if count == 6:
-        hvio=[]
-        for item in table.findAll('td'):
-            hvio.append(item.renderContents())
-        allhvio.append(hvio)
-    if count == 5:
-        mrvio=[]
-        for item in table.findAll('td'):
-            mrvio.append(item.renderContents())
-        allmrvio.append(mrvio)
+    #goes into the page and finds the tables of health and reporting violations
+    allhvio=[]
+    allmrvio=[]
+    for table in soup.findAll('table', {'class':'result2'}):
+        count=0
+        for item in table.findAll('th'):
+            count+=1
+        if count == 6: #checks if table is a health violation
+            hvio=[wid]
+            for item in table.findAll('td'):
+                hvio.append(item.renderContents()) #builds a list of all data for that violation
+            allhvio.append(hvio) #builds list of lists of all violations and their data
+        if count == 5: #checks if table is a reporting violation
+            mrvio=[wid]
+            for item in table.findAll('td'):
+                mrvio.append(item.renderContents())
+            allmrvio.append(mrvio)
 
-dfh=pd.DataFrame(allhvio,columns=['vio_type','date_begin','date_end','water_rule','analytics','vio_id'])
-dfmr=pd.DataFrame(allmrvio,columns=['vio_type','date_begin','date_end','water_rule','vio_id'])
+    #builds dataframe from teh lists of lists generated,
+    dfh=pd.DataFrame(allhvio,columns=['wid','vio_type','date_begin','date_end','water_rule','analytics','vio_id'])
+    dfmr=pd.DataFrame(allmrvio,columns=['wid','vio_type','date_begin','date_end','water_rule','vio_id'])
+
+    return dfh,dfmr
+
+allhealth=pd.DataFrame(columns=['wid','vio_type','date_begin','date_end','water_rule','analytics','vio_id'])
+allreporting=pd.DataFrame(columns=['wid','vio_type','date_begin','date_end','water_rule','vio_id'])
+for i in sdwis.index:
+    health,reporting=viodata(i)
+    allhealth=pd.concat([allhealth,health])
+    allreporting=pd.concat([allreporting,reporting])
+
+
+allhealth
+allreporting
