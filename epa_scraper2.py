42f2282fec00f3c9440fd75250326f620219e233
Adam Sulier <absulier@gmail.com>
2016-05-20 18:46:06 -0400

    scraped some

--- epa_scraper2.py
+++ epa_scraper2.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from sys import setrecursionlimit as srl
 srl(500000)
 
-sdwis=pd.read_csv('df1.csv')
+sdwis=pd.read_csv('df5.csv')
 #function takes an index from the dataframe of water treatment plants
 #uses that index to find the wid and link to page on violations
 #creats two dataframes of health violations and reporting violations
@@ -49,5 +49,5 @@ for i in sdwis.index:
     allhealth=pd.concat([allhealth,health])
     allreporting=pd.concat([allreporting,reporting])
 
-pd.DataFrame.to_csv(allhealth,"health_violations1.csv")
-pd.DataFrame.to_csv(allreporting,"reporting_violations1.csv")
+pd.DataFrame.to_csv(allhealth,"health_violations5.csv")
+pd.DataFrame.to_csv(allreporting,"reporting_violations5.csv")
--- splitter.py
+++ splitter.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.cross_validation import train_test_split as tts
 
-
+df=pd.read_csv('sdwis_clean.csv')
+df=df.drop(['Unnamed: 0'], axis=1)
 
 df,df1=tts(df,train_size=.9)
 df,df2=tts(df, train_size=.89)
@@ -12,3 +13,17 @@ df,df6=tts(df, train_size=.8)
 df,df7=tts(df, train_size=.75)
 df,df8=tts(df, train_size=.66)
 df10,df9=tts(df,train_size=.5)
+
+
+pd.DataFrame.to_csv(df1,"df1.csv")
+pd.DataFrame.to_csv(df2,"df2.csv")
+pd.DataFrame.to_csv(df3,"df3.csv")
+pd.DataFrame.to_csv(df4,"df4.csv")
+pd.DataFrame.to_csv(df5,"df5.csv")
+pd.DataFrame.to_csv(df6,"df6.csv")
+pd.DataFrame.to_csv(df7,"df7.csv")
+pd.DataFrame.to_csv(df8,"df8.csv")
+pd.DataFrame.to_csv(df9,"df9.csv")
+pd.DataFrame.to_csv(df10,"df10.csv")
+
+len(df1)
