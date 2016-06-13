832684574266b5bca491a32254fbfba8af9c2166
Adam Sulier <absulier@gmail.com>
2016-06-13 16:03:23 -0400

    added date analysis on violations

--- vio_organizer.py
+++ vio_organizer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 df=pd.read_csv('../violations.csv', low_memory=False)
 
@@ -73,3 +74,63 @@ for i in df.index:
 df['lead']=lead
 df['e_coli']=ecoli
 df['coliform']=coliform
+
+#working with dates
+date=df[['date_begin','date_end']]
+
+y_b=[]
+m_b=[]
+d_b=[]
+for i in date.date_begin:
+    if len(i)==11:
+        y_b.append(i[-4:])
+        m_b.append(i[0:3])
+        d_b.append(i[4:6])
+    else:
+        y_b.append('---')
+        m_b.append('---')
+        d_b.append('---')
+
+y_e=[]
+m_e=[]
+d_e=[]
+for i in date.date_end:
+    if len(i)==11:
+        y_e.append(i[-4:])
+        m_e.append(i[0:3])
+        d_e.append(i[4:6])
+    else:
+        y_e.append('---')
+        m_e.append('---')
+        d_e.append('---')
+
+date.head()
+len(date)
+
+months=['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+num=[1,2,3,4,5,6,7,8,9,10,11,12]
+conve=pd.DataFrame({'months':months,'num':num})
+
+date['y_b']=y_b
+date['months']=m_b
+date=date.merge(conve, how='left', on='months').drop('months',axis=1)
+date=date.rename(columns={'num':'m_b'})
+date['d_b']=d_b
+
+date['y_e']=y_e
+date['months']=m_e
+date=date.merge(conve, how='left', on='months').drop('months',axis=1)
+date=date.rename(columns={'num':'m_e'})
+date['d_e']=d_e
+
+length=[]
+for i in date.index:
+    if len(date.y_b[i])==4 and len(date.y_e[i])==4:
+        start=datetime(int(date.y_b[i]),int(date.m_b[i]),int(date.d_b[i]))
+        end=datetime(int(date.y_e[i]),int(date.m_e[i]),int(date.d_e[i]))
+        gap=end-start
+        length.append(gap.days)
+    else:
+        length.append('---')
+
+df['length_days']=length
