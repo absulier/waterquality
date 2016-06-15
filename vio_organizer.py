d91256ca9cfae4b929e3bbb3f61caaf80bfdf077
Adam Sulier <absulier@gmail.com>
2016-06-15 13:15:51 -0400

    cleaned data and organized violations

--- vio_organizer.py
+++ vio_organizer.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime
+import warnings
+import math
+warnings.filterwarnings("ignore")
 
 df=pd.read_csv('../violations.csv', low_memory=False)
 
@@ -76,8 +80,10 @@ df['e_coli']=ecoli
 df['coliform']=coliform
 
 #working with dates
+#builds a database to hold data information
 date=df[['date_begin','date_end']]
 
+#parses out the string into year, month, and day
 y_b=[]
 m_b=[]
 d_b=[]
@@ -90,7 +96,7 @@ for i in date.date_begin:
         y_b.append('---')
         m_b.append('---')
         d_b.append('---')
-
+#does the same parse, but for the end date
 y_e=[]
 m_e=[]
 d_e=[]
@@ -104,25 +110,29 @@ for i in date.date_end:
         m_e.append('---')
         d_e.append('---')
 
-date.head()
-len(date)
-
+#builds a new data frame so to convert months to numbers
 months=['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
 num=[1,2,3,4,5,6,7,8,9,10,11,12]
 conve=pd.DataFrame({'months':months,'num':num})
 
+#puts the year and month into data frame, then merges numbers for month,
+#then drops the non numeric month, then adds days
 date['y_b']=y_b
 date['months']=m_b
 date=date.merge(conve, how='left', on='months').drop('months',axis=1)
 date=date.rename(columns={'num':'m_b'})
 date['d_b']=d_b
 
+#repeats for end dates
 date['y_e']=y_e
 date['months']=m_e
 date=date.merge(conve, how='left', on='months').drop('months',axis=1)
 date=date.rename(columns={'num':'m_e'})
 date['d_e']=d_e
 
+date
+
+#finds the lengths of violations
 length=[]
 for i in date.index:
     if len(date.y_b[i])==4 and len(date.y_e[i])==4:
@@ -133,4 +143,66 @@ for i in date.index:
     else:
         length.append('---')
 
-df['length_days']=length
+df['length']=length
+
+df=df.drop(['date_begin','date_end','vio_id','city','vio_type','water_rule'],axis=1)
+df=df.merge(pd.get_dummies(df.vio_class),how='left',left_index=True,right_index=True).drop('vio_class',axis=1)
+
+
+def to_nan(x):
+    if x == '---':
+        return np.nan
+    else:
+        return x
+
+for i in df.columns:
+    df[i]=df[i].apply(to_nan)
+
+to_float = lambda x : float(x)
+df.analytics=df.analytics.apply(to_float)
+
+df1=df.groupby(by=['wid'],as_index=False)
+df1=df1.agg({'analytics':np.mean,
+            'county':lambda x: x.iloc[0],
+            'state':lambda x: x.iloc[0],
+            'population':lambda x: x.iloc[0],
+            'watertype':lambda x: x.iloc[0],
+            '2006':np.sum,
+            '2007':np.sum,
+            '2008':np.sum,
+            '2009':np.sum,
+            '2010':np.sum,
+            '2011':np.sum,
+            '2012':np.sum,
+            '2013':np.sum,
+            '2014':np.sum,
+            '2015':np.sum,
+            '2016':np.sum,
+            'unknown/old':np.sum,
+            'lead':np.sum,
+            'e_coli':np.sum,
+            'coliform':np.sum,
+            'length':np.mean,
+            'health':np.sum,
+            'reporting':np.sum
+            })
+
+df1=df1[['wid','state','county','population','watertype','health','reporting',
+    'length','analytics','lead','e_coli','coliform','2006','2007','2008','2009',
+    '2010','2011','2012','2013','2014','2015','2016','unknown/old']]
+
+def nan_0(x):
+    if isinstance(x,float):
+        if math.isnan(x):
+            return 0
+        else:
+            return x
+    else:
+        return x
+
+for i in df.columns:
+    df1[i]=df1[i].apply(nan_0)
+
+df1.head()
+
+pd.DataFrame.to_csv(df1,'vio_org.csv')
